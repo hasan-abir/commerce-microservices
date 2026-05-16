e93e4bde4704e9d1d831a501fbb71e5c6576bc7b
hasan-abir <contact.hsanabir@gmail.com>
2026-05-16 14:30:10 +0600

    place order func

--- commerceproject/urls.py
+++ commerceproject/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 from checkout_api.views import ProductListView, PlaceOrderView, StripeWebhookView, ClientHomeView
 
 urlpatterns = [
-    # path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('', ClientHomeView.as_view()),
     path('api/checkout/demo-products/', ProductListView.as_view(), name="product-list"),
     path('api/checkout/place-order/', PlaceOrderView.as_view(), name="place-order"),
