c154853a162bffdb2e4d13528b0d7b10dc239fca
hasan-abir <contact.hsanabir@gmail.com>
2026-07-11 22:33:06 +0600

    cart UI overhaul

--- start_prerequisites.bat
+++ start_prerequisites.bat
@@ -3,17 +3,17 @@ rem Script description: WINDOWS ONLY. CHANGE THE DIRECTORIES AS NEEDED
 
 call "%~dp0stop_prerequisites.bat"
 
-cd /d G:\postgresql-18.4-1-windows-x64-binaries\pgsql\bin
-@REM cd /d I:\postgresql-18.1-2-windows-x64-binaries\pgsql\bin
-pg_ctl -D "G:\postgresql-18.4-1-windows-x64-binaries\db" -l logfile start
-@REM pg_ctl -D "I:\postgresql-18.1-2-windows-x64-binaries\data" -l logfile start
+@REM cd /d G:\postgresql-18.4-1-windows-x64-binaries\pgsql\bin
+cd /d I:\postgresql-18.1-2-windows-x64-binaries\pgsql\bin
+@REM pg_ctl -D "G:\postgresql-18.4-1-windows-x64-binaries\db" -l logfile start
+pg_ctl -D "I:\postgresql-18.1-2-windows-x64-binaries\data" -l logfile start
 
-cd G:\Redis-8.6.3-Windows-x64-msys2
-@REM cd I:\Redis-x64-5.0.14
+@REM cd G:\Redis-8.6.3-Windows-x64-msys2
+cd I:\Redis-x64-5.0.14
 start "" redis-server.exe
 
-cd G:\
-@REM cd I:\
+@REM cd G:\
+cd I:\
 start "" MailHog_windows_amd64.exe
 
 setlocal
